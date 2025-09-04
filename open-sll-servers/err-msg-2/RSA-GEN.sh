e7cec9b4648fe0a140b46155112a5ddbc65d3ee2
DanielKnebel <danielknebel@mail.tau.ac.il>
2025-09-04 17:58:10 +0300

    seems to work fine

--- open-sll-servers/err-msg-2/RSA-GEN.sh
+++
@@ -1,5 +0,0 @@
-# Generate 2048-bit RSA private key
-/usr/local/openssl-1.0.2g/bin/openssl genrsa -out server.key 2048
-
-# Generate self-signed certificate
-/usr/local/openssl-1.0.2g/bin/openssl req -x509 -new -key server.key -out server.cert -days 365 -subj "/CN=localhost"
